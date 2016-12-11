f07b37166899a11ef072ee2754abe34d697a264b
Wes Billman <wes@compass.com>
2016-12-11 16:18:59 -0700

    Add Just stuff

---
+++ Snail/Just.swift
@@ -0,0 +1,21 @@
+//  Copyright © 2016 Compass. All rights reserved.
+
+import Foundation
+
+public class Just<T>: Observable<T> {
+    private let value: T
+
+    public init(value: T) {
+        self.value = value
+    }
+
+    public override func subscribe(_ handler: @escaping (Event<T>) -> Void) {
+        handler(.next(value))
+        handler(.done)
+    }
+
+    public override func subscribe(onNext: ((T) -> Void)?, onError: ((Error) -> Void)?, onDone: (() -> Void)?) {
+        onNext?(value)
+        onDone?()
+    }
+}
