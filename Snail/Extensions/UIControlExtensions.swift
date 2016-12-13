ee84544fbbf5e214547feb95a63b71b71314d58a
Wes Billman <wes@compass.com>
2016-12-13 08:49:11 -0700

    Always fire controlEvent on main

--- Snail/Extensions/UIControlExtensions.swift
+++ Snail/Extensions/UIControlExtensions.swift
@@ -12,7 +12,9 @@ public extension UIControl {
 
     func observableHandler(_ sender: UIControl) {
         if let observable = objc_getAssociatedObject(self, &UIControl.observableKey) as? Observable<Void> {
-            observable.on(.next())
+            DispatchQueue.main.async {
+                observable.on(.next())
+            }
         }
     }
 }
