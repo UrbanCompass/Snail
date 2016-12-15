2c2eede9b20b84980ee20cf67df715e7a827b2db
Russell Stephens <russell.stephens@compass.com>
2016-12-15 13:06:52 -0500

    Adding bar button extensions

---
+++ Snail/Extensions/UIBarButtonItemExtensions.swift
@@ -0,0 +1,21 @@
+//  Copyright © 2016 Compass. All rights reserved.
+
+import UIKit
+
+extension UIBarButtonItem {
+    private static var observableKey = "ObservableKey"
+
+    public var tap: Observable<Void> {
+        let observable = Observable<Void>()
+        objc_setAssociatedObject(self, &UIBarButtonItem.observableKey, observable, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        target = self
+        action = #selector(observableHandler(_:))
+        return observable
+    }
+
+    @objc private func observableHandler(_ sender: UIBarButtonItem) {
+        if let observable = objc_getAssociatedObject(self, &UIBarButtonItem.observableKey) as? Observable<Void> {
+            observable.on(.next())
+        }
+    }
+}
