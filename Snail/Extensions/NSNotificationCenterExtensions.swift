05ce91168d31a990729cbfc8c65336ac8b314f54
Russell Stephens <russell.stephens@compass.com>
2016-12-20 11:45:50 -0500

    adding notification extension

---
+++ Snail/Extensions/NSNotificationCenterExtensions.swift
@@ -0,0 +1,20 @@
+//  Copyright © 2016 Compass. All rights reserved.
+
+import Foundation
+
+extension NotificationCenter {
+    private static var observableKey = "ObservableKey"
+
+    func observableHandler(_ notification: NSNotification) {
+        if let observable = objc_getAssociatedObject(self, &NotificationCenter.observableKey) as? Observable<Void> {
+            observable.on(.next())
+        }
+    }
+
+    func observeEvent(name: NSNotification.Name?) -> Observable<Void> {
+        let observable = Observable<Void>()
+        objc_setAssociatedObject(self, &NotificationCenter.observableKey, observable, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        addObserver(self, selector: #selector(observableHandler(_:)), name: name, object: nil)
+        return observable
+    }
+}
