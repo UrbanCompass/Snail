e895436189d2cdedad905295da1657bedbb49c88
Russell Stephens <russell.stephens@compass.com>
2017-01-05 10:13:14 -0500

    hooking up subscribers

--- SnailTests/FailTests.swift
+++ SnailTests/FailTests.swift
@@ -10,18 +10,14 @@ class FailTests: XCTestCase {
     }
 
     private var subject: Observable<String>?
-    private var strings: [String]?
+    private var strings: [String] = []
     private var error: Error?
     private var done: Bool?
 
     override func setUp() {
         super.setUp()
         subject = Fail(TestError.test)
-        strings = []
-    }
-
-    override func tearDown() {
-        super.tearDown()
+        subject?.subscribe(onNext: { [weak self] string in self?.strings.append(string) })
     }
 
     func testEvent() {
@@ -34,21 +30,21 @@ class FailTests: XCTestCase {
             }
         }
         subject?.on(.next("1"))
+        XCTAssert(strings.count == 0)
         XCTAssert(more.count == 0)
     }
 
     func testOnNext() {
         subject?.on(.next("1"))
         subject?.on(.next("2"))
-        XCTAssert(strings?.count == 0)
+        XCTAssert(strings.count == 0)
     }
 
     func testOnError() {
         subject?
             .subscribe(onError: { error in self.error = error })
         subject?.on(.next("1"))
-
-        XCTAssert(strings?.count == 0)
+        XCTAssert(strings.count == 0)
         XCTAssert((error as? TestError) == TestError.test)
     }
 }
