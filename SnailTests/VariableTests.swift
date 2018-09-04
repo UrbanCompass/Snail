2d2b156930d4d9cda74763ae12a8cb46b4670062
Russell Stephens <russell.stephens@compass.com>
2018-09-04 13:33:25 -0400

    add test for variable map

--- SnailTests/VariableTests.swift
+++ SnailTests/VariableTests.swift
@@ -78,6 +78,19 @@ class VariableTests: XCTestCase {
         XCTAssertTrue(firedCount == 1)
     }
 
+    func testVariableFireCounts() {
+        let subject = Variable("one")
+        var firedCount = 0
+
+        subject.map { $0.count }.asObservable().subscribe(onNext: { _ in
+            firedCount += 1
+        })
+
+        subject.value = "two"
+
+        XCTAssertTrue(firedCount == 2)
+    }
+
     func testMapToVoid() {
         let subject = Variable("initial")
         var fired = false
