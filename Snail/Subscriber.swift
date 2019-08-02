3c0cb7b500e86fec2e5cb8d5ae49d85116d03dc5
Lucas Lain <lucas.lain@gmail.com>
2019-08-02 14:38:20 -0400

    Private set

--- Snail/Subscriber.swift
+++ Snail/Subscriber.swift
@@ -5,7 +5,7 @@ import Foundation
 public class Subscriber<T>: DisposableType {
     let queue: DispatchQueue?
     let handler: (Event<T>) -> Void
-    public weak var observable: Observable<T>?
+    private(set) public weak var observable: Observable<T>?
 
     public init(queue: DispatchQueue?, observable: Observable<T>, handler: @escaping (Event<T>) -> Void) {
         self.queue = queue
