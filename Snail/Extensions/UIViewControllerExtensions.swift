3bb25ef17d57b623c40c03a7ed6b548922d4c151
Lucas Lain <lucas.lain@gmail.com>
2019-09-06 20:43:46 -0400

    Using VC main view disposer as VC Disposer

--- Snail/Extensions/UIViewControllerExtensions.swift
+++ Snail/Extensions/UIViewControllerExtensions.swift
@@ -7,14 +7,7 @@ import Foundation
 #endif
 
 extension UIViewController {
-    private static var disposerKey = "com.compass.Snail.UIViewController.disposer"
-
     public var disposer: Disposer {
-        if let disposer = objc_getAssociatedObject(self, &UIViewController.disposerKey) as? Disposer {
-            return disposer
-        }
-        let disposer = Disposer()
-        objc_setAssociatedObject(self, &UIViewController.disposerKey, disposer, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
-        return disposer
+        return view.disposer
     }
 }
