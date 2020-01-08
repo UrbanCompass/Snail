d11c24d404b1562550a788f9fde3c47259c57edc
Alan Wang <alan.wang@compass.com>
2020-01-08 13:14:00 -0500

    Send same things as Apple in observable

--- Snail/Extensions/UIGestureRecognizerExtensions.swift
+++ Snail/Extensions/UIGestureRecognizerExtensions.swift
@@ -6,19 +6,19 @@ import UIKit
 extension UIGestureRecognizer {
     private static var observableKey = "com.compass.Snail.UIGestureRecognizer.ObservableKey"
 
-    public func asObservable() -> Observable<UIGestureRecognizer.State> {
-        if let observable = objc_getAssociatedObject(self, &UIGestureRecognizer.observableKey) as? Observable<UIGestureRecognizer.State> {
+    public func asObservable() -> Observable<(UIGestureRecognizer, UIEvent)> {
+        if let observable = objc_getAssociatedObject(self, &UIGestureRecognizer.observableKey) as? Observable<(UIGestureRecognizer, UIEvent)> {
             return observable
         }
-        let observable = Observable<UIGestureRecognizer.State>()
+        let observable = Observable<(UIGestureRecognizer, UIEvent)>()
         objc_setAssociatedObject(self, &UIGestureRecognizer.observableKey, observable, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
         addTarget(self, action: #selector(observableHandler))
         return observable
     }
 
-    @objc private func observableHandler() {
-        if let observable = objc_getAssociatedObject(self, &UIGestureRecognizer.observableKey) as? Observable<UIGestureRecognizer.State> {
-            observable.on(.next(state))
+    @objc private func observableHandler(sender: UIGestureRecognizer, event: UIEvent) {
+        if let observable = objc_getAssociatedObject(self, &UIGestureRecognizer.observableKey) as? Observable<(UIGestureRecognizer, UIEvent)> {
+            observable.on(.next((sender, event)))
         }
     }
 }
