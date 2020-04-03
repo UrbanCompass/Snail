b36259f2c60a48342080d8a69c3409cc71f24db8
Russell Stephens <russell.stephens@compass.com>
2020-04-03 15:50:17 -0400

    name args

--- scripts/template.sh
+++ scripts/template.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-template="$(cat $1)"
+template=$1
+output=$2
+
+template="$(cat $template)"
 template=$(sed 's/\([^\\]\)"/\1\\"/g; s/^"/\\"/g' <<< "$template")
-eval "echo \"${template}\"" > $2
+eval "echo \"${template}\"" > $output
