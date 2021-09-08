e506ef57039fe9fd301cff5cdc0717d3ce69b398
Tim Chamberlin <timothy.chamberlin@compass.com>
2021-09-08 16:45:34 -0400

    Dispose on deinit Use NSRecursiveLock for subscriber reads/writes

--- Snail/Disposer.swift
+++ Snail/Disposer.swift
@@ -7,16 +7,23 @@ public protocol DisposableType {
 }
 
 public class Disposer {
-    private(set) public var disposables: [DisposableType] = []
+    private(set) var disposables: [DisposableType] = []
+    private let recursiveLock = NSRecursiveLock()
 
     public init() {}
 
+    deinit {
+        disposeAll()
+    }
+
     public func disposeAll() {
+        recursiveLock.lock(); defer { recursiveLock.unlock() }
         disposables.forEach { $0.dispose() }
         disposables.removeAll()
     }
 
     public func add(disposable: DisposableType) {
+        recursiveLock.lock(); defer { recursiveLock.unlock() }
         disposables.append(disposable)
     }
 }
